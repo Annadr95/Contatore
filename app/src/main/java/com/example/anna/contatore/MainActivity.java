ea6b4ba76a5eda38310151d7180eb6a829083c2b
Anna de Rosa <anna.derosa5@studenti.unicampania.it>
2018-03-26 16:02:58 +0200

    versione funzionante

--- app/src/main/java/com/example/anna/contatore/MainActivity.java
+++ app/src/main/java/com/example/anna/contatore/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.anna.contatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // view layout
+    private TextView vMessaggio;
+    private Button vAumenta;
+    private Button vDiminuisci;
+
+    private int contatore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //imposto riferimenti layout
+        vMessaggio = findViewById(R.id.textMessaggio);
+        vAumenta = findViewById(R.id.btnAumenta);
+        vDiminuisci = findViewById(R.id.btndiminuisci);
+
+        //imposto azioni pulsanti
+        vAumenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ++contatore;
+                visualizzaMessaggio();
+            }
+        });
+
+        vDiminuisci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                --contatore;
+                visualizzaMessaggio();
+            }
+        });
+    }
+
+    /**
+     * funzione che visualizza il valore del contatore
+     */
+    private void visualizzaMessaggio(){
+        vMessaggio.setText((R.string.valore)+contatore);
     }
 }
